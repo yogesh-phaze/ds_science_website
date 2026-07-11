b7bd784ec8b471e728383016579a3525f4d5f6aa
VirendraIITD <virendra.yadav@phazeai.in>
2026-07-11 12:08:47 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.site-nav');
-if (navToggle && navMenu) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('open');
-  });
-}
